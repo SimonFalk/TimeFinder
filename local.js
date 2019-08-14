3a87199d76902cafc3e88f69ceaf3e0ee200ee50
KidFalk <kidfalk@n149-p173.eduroam.kth.se>
2019-08-14 13:53:32 +0200

    Inserted comments, removed obsolete functions

--- local.js
+++ local.js
@@ -1,6 +1,6 @@
-localStorage.setItem('issues', JSON.stringify([]));
-const m = document.getElementById("myTable").rows.length - 1
-const n = document.getElementById("myTable").rows[0].cells.length - 1
+localStorage.setItem('storage', JSON.stringify([]));
+const m = document.getElementById("calendar").rows.length - 1
+const n = document.getElementById("calendar").rows[0].cells.length - 1
 
 document.querySelector('table').onclick = function() {
 	let td = event.target.closest('td');
@@ -9,50 +9,45 @@ document.querySelector('table').onclick = function() {
 
 	if (!this.contains(td)) return;
 
-	let invBtn = document.getElementById("checkbox").checked;
-	if (invBtn==false) {
-		highlight(td,'free');
+	// The cell selection can be inverted, if selectRed is true, slots turn busy when clicking
+	let selectRed = document.getElementById("checkbox").checked;
+	if (selectRed==false) {
+		td.classList.toggle('free');
 	} else {
-		highlight(td,'occupied')
+		td.classList.toggle('occupied');
 	}
 }
 
-function highlight(td,cl) {
-	// Tag version:
-	//
-	// if (td.getAttribute('occupied') == "true") {
-	// 	td.setAttribute('occupied',"false");
-	// } else if (td.getAttribute('occupied') == "false" | td.hasAttribute('occupied') == "false") {
-	// 	td.setAttribute('occupied',"true");
-	// };
-
-	td.classList.toggle(cl);
-}
-
 function tdFromIndex(index) {
+	// Returns the td element with the specified index
+	// Cells are ordered first by column then by row (same dates first)
 	let col = Math.floor(index/m) + 1;
 	let row = index%m + 1;
-	return document.getElementById("myTable").rows[row].cells[col]
+	return document.getElementById("calendar").rows[row].cells[col]
 }
 
 
 
-function gather(save) {
-	let invBtn = document.getElementById("checkbox").checked;
-	let table = document.getElementById("myTable");
+function getBinArray(save) {
+	// returns the binary array of free slots (if save==true)
+	// clears all formatting of the calendar table
+	let selectRed = document.getElementById("checkbox").checked;
+	let table = document.getElementById("calendar");
 	let localArr = [];
     if (table != null) {
         for (var col = 1; col < table.rows[0].cells.length; col++) {
             for (var row = 1; row < table.rows.length; row++){
+            	// Loops through all cells:
                 cell = table.rows[row].cells[col];
+				
                 if (save == true){
-                	if (invBtn==true) {
-                		localArr.push(Number(!cell.classList.contains('occupied')));
-                	}else{
-                		localArr.push(Number(cell.classList.contains('free')));
-                	}
-                	//localArr.push(cell.getAttribute('occupied')); tag version
                 	
+                	if (selectRed==true) {
+
+                		localArr.push(Number(!cell.classList.contains('occupied')));
+                	} else {
+                		localArr.push(Number(cell.classList.contains('free')));
+                	}	
 
             	}
                 cell.classList.remove('occupied');
@@ -66,100 +61,107 @@ function gather(save) {
 }
 
 function resetSpace() {
-	gather(false);
+	// Removes formatting and resets the form
+	getBinArray(false);
 	document.getElementById('occForm').reset();
 }
 
 function savePost() {
-
 	var postName = document.getElementById('nameInput').value;
-  	var postArr = gather(true);
+  	var postArr = getBinArray(true);
+  	//console.log(postArr);
   	var count = 0;
+
+  	// Creates a html representation of cells to append to the submissions table
   	var html = "";
 		for(var i = 0; i < postArr.length; ++i){
 			count += Boolean(postArr[i]);
+
+			// Makes green cell if free, red cell if not
 			if (Boolean(postArr[i]) == true) {
 				html +='<td width="1px" class="free thin"></td>'
 			} else {
 				html +='<td width="1px" class="occupied thin"></td>'
-			}
+		}
 	}
+
+	// Creates post object with name, binary array and number of free slots
   	var post = {
     	name: postName,
     	arr: postArr,
     	freeSlots: count,
   	}
 
-  	if (localStorage.getItem('issues') == "[]") {
+  	// Store in localStorage together with histogram, which is array 
+  	// with current no of bookings of each time slot
+  	if (localStorage.getItem('storage') == "[]") {
     	var posts = [];
     	posts.push(post);
-    	var issues = {
+    	var storage = {
     		posts: posts,
-    		total: post.arr,
+    		histogram: post.arr,
     	};
-    	localStorage.setItem('issues', JSON.stringify(issues));
+    	localStorage.setItem('storage', JSON.stringify(storage));
     	document.getElementById("postTable").style.visibility = "visible";
   	} else {
-    	var issues = JSON.parse(localStorage.getItem('issues'));
-    	issues.posts.push(post);
-    	for (var i=0; i<issues.total.length;i++) {
-	    		issues.total[i] += post.arr[i]
+    	var storage = JSON.parse(localStorage.getItem('storage'));
+    	storage.posts.push(post);
+    	for (var i=0; i<storage.histogram.length;i++) {
+	    		storage.histogram[i] += post.arr[i]
 	    }
-    	localStorage.setItem('issues', JSON.stringify(issues));
+    	localStorage.setItem('storage', JSON.stringify(storage));
   	}
-
 
   	var postList = document.getElementById('postList');
   	var dimension = m*n;
   	postList.innerHTML +=  '<tr><td>'+postName+'</td><td>'+count+'</td>'+html+'</tr>';
-  	document.getElementById('scheduleHead').setAttribute('colspan','28');
-	  
 	document.getElementById('occForm').reset();
   
-  	//e.preventDefault(); 
 }
 
-function computeOptimal(size) {
-	let radio1 = document.getElementById("radio1");
-	let radio2 = document.getElementById("radio2");
-	let groupSize = Number(radio1.checked) + 2*Number(radio2.checked);
-
-	if (localStorage.getItem('issues') == "[]") {
+function computeOptimal() {
+	if (localStorage.getItem('storage') == "[]") {
     	alert('No submissions made yet!');
     	return
   	}
-	let issues = JSON.parse(localStorage.getItem('issues'));
-	let total = issues.total;
+  	/*
+  	let hours = prompt("How many hours are needed for the occasion?",1);
+
+  	if(!Number.isInteger(hours)) { // Default value if bad answer is received
+  		hours = 1;
+  	}
+  	*/
+  	let hours = 1;
+
+	let storage = JSON.parse(localStorage.getItem('storage'));
+	// histogram is array with no of bookings of each time slot
+	let histogram = storage.histogram;
 
 	// Find largest element in list
 	let largestVal = [0];
+	// largestInd contains the indices with highest no. of bookings
 	let largestInd = [];
-	let groupTotal = total
-	for (let i=0;i<total.length;i++){
-		for (let j=1;j<groupSize;j++) {
-			groupTotal[i] += total[i+j];
-		}
-		if (groupTotal[i]>largestVal[0]) {
-			largestVal = [groupTotal[i]];
+
+	for (let i=0;i<histogram.length;i++){
+
+		if (histogram[i]>largestVal[0]) {
+			largestVal = [histogram[i]];
 			largestInd = [i];
-		} else if (groupTotal[i]==largestVal[0]) {
-			largestVal.push(groupTotal[i]);
+		} else if (histogram[i]==largestVal[0]) {
+			largestVal.push(histogram[i]);
 			largestInd.push(i);
 		}
 	}
-	if (groupSize == 1) {
-	largestInd.forEach(function(index) {
-			highlight(tdFromIndex(index),'winner');
-		})
-	} else {
-		for (let j=0;j<groupSize;j++) {
-			highlight(tdFromIndex(largestInd[0]+j),'winner');	
-		}
-	}
 
-	
-	
+	// Multiple winners may be chosen
+	largestInd.forEach(function(value) {
+		tdFromIndex(value).classList.add('winner');
+	})
 }
+
+
+
+
 
 function about() {
 	document.getElementById("aboutP").classList.remove('isHidden');
